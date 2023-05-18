24a272c70e0e201f6996da51303d9e668eba26ed
Quentin Rollet <rollet.qunetin@orange.fr>
2023-05-18 21:33:45 +0200

    Add all the pages for the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,13 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import DataChallengeDetail from "./pages/DataChallengeDetail";
 import MyDataChallenges from "./pages/MyDataChallenges";
+import DataChallengeRendu from "./pages/DataChallengeRendu";
+import TeamView from "./pages/TeamView";
+import Profile from "./pages/Profile";
+import MCQ from "./pages/MCQ";
+import EvaluateMCQ from "./pages/EvaluateMCQ";
+import DataChallengeEdition from "./pages/DataChallengeEdition";
+import Admin from "./pages/Admin";
 
 function App() {
     return (
@@ -14,18 +21,49 @@ function App() {
             <Header />
             <Routes>
                 <Route exact path="" element={<Home />} />
+
                 <Route exact path="login" element={<Login />} />
                 <Route exact path="signup" element={<Signup />} />
-                <Route
-                    exact
-                    path="data-challenges"
-                    element={<MyDataChallenges />}
-                />
+
+                <Route exact path="profile/:id_profile" element={<Profile />} />
+
                 <Route
                     exact
                     path="data-challenges/:id"
                     element={<DataChallengeDetail />}
                 />
+                <Route
+                    exact
+                    path="my-data-challenges"
+                    element={<MyDataChallenges />}
+                />
+                <Route
+                    exact
+                    path="my-data-challenges/:id_equipe/:id_data_challenge"
+                    element={<DataChallengeRendu />}
+                />
+
+                <Route exact path="team-view/:id_team" element={<TeamView />} />
+
+                <Route
+                    exact
+                    path="MCQ/:id_MCQ/:id_question"
+                    element={<MCQ />}
+                />
+                <Route
+                    exact
+                    path="evaluate_MCQ/:id_MCQ/:id_team"
+                    element={<EvaluateMCQ />}
+                />
+
+                <Route
+                    exact
+                    path="data-challenges/edit/:id_data_challenge"
+                    element={<DataChallengeEdition />}
+                />
+
+                <Route exact path="admin" element={<Admin />} />
+
                 <Route path="*" element={<Error404 />} />
             </Routes>
         </Router>
