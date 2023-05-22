80c7f38e9f71a6797b167e0f8a0a8f75a0784886
Quentin Rollet <rollet.quentin@orange.fr>
2023-05-22 18:28:32 +0200

    header + homepage
    Adapting the header belong the user role & create the homepage

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,5 +1,35 @@
+import styled from "styled-components";
+
+import routes from "../../utils/routes";
+
+import ButtonLink from "../../components/ButtonLink";
+import DataChallengeItem from "../../components/DataChallengeItem";
+
+const StyledHome = styled.div`
+    border: 1px solid red;
+    width: 500px;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    & .data-challenge {
+        background: red;
+        margin: 10px 0px 10px 0px;
+    }
+`;
+
 function Home() {
-    return <div>Home</div>;
+    return (
+        <StyledHome>
+            <h2>Data Challenges</h2>
+
+            <DataChallengeItem title="Data Challenge 1" id="1" />
+            <DataChallengeItem title="Data Challenge 2" id="2" />
+            <DataChallengeItem title="Data Challenge 3" id="3" />
+        </StyledHome>
+    );
 }
 
 export default Home;
