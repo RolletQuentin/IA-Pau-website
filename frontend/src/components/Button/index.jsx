cf06cfe30f9a3367fbc702648e0834508891c17f
Quentin Rollet <rollet.qunetin@orange.fr>
2023-05-23 15:58:25 +0200

    my data challenges page + correction bug

--- frontend/src/components/Button/index.jsx
+++ frontend/src/components/Button/index.jsx
@@ -1,25 +1,18 @@
 import styled from "styled-components";
 
-const StyledButton = styled.button`
+const StyledButton = styled.div`
+    display: flex;
+    align-items: center;
     width: 100%;
     background: var(--background-color);
     color: var(--font-color);
-    width: 100%;
     height: 80px;
-    padding: 30px 0px 30px 0px;
+    padding: 0px 30px 0px 30px;
     border-radius: 30px;
-    border: 0px;
     box-shadow: 1px 4px 4px 0px rgba(0, 0, 0, 0.25);
 `;
-function Button({
-    children
-}) {
-
-    return (
-        <StyledButton>
-            {children}
-        </StyledButton>
-    );
+function Button({ children, className }) {
+    return <StyledButton className={className}>{children}</StyledButton>;
 }
 
 export default Button;
