e380ae92c444a0c14740a926451d85bb71486dd7
Quentin Rollet <rollet.quentin@orange.fr>
2023-05-23 15:58:25 +0200

    my data challenges page + correction bug

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,12 +1,14 @@
 import LoginForm from "../../components/Form/Login";
+import NavbarOffset from "../../components/NavbarOffset";
 import CenterContainer from "../../containers/CenterContainer";
 
 function Login() {
     return (
-        <CenterContainer style={{minWidth: "500px"}}>
+        <CenterContainer style={{ minWidth: "500px" }}>
+            <NavbarOffset />
             <LoginForm />
         </CenterContainer>
-    )
+    );
 }
 
 export default Login;
