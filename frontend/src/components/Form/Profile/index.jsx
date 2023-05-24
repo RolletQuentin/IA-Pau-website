aa608411b11dc28b3ca8d9ed17b42f3a532ce644
Hadronoxx <maxime_dejardin@orange.fr>
2023-05-24 15:41:17 +0200

    signup profile

--- frontend/src/components/Form/Profile/index.jsx
+++ frontend/src/components/Form/Profile/index.jsx
@@ -8,10 +8,84 @@ import BasicButton from "../../BasicButton";
 import styled from "styled-components";
 
 
-const Profile = () => {
-    return(
+const StyledSelect = styled.select`
+    flex-grow: 1;
+    border-radius: 10px;
+    padding-bottom: 20px;
+    background-color: var(--primary);
+    padding: 10px;
+    border : none;
+    color : rgb(117,117,117);
+`;
 
-        <div style={{margin : "150px"}}>hello</div>
 
+const styleInputText = {
+    flexGrow: 1
+}
+
+const styleOption = {
+    justifyContent: "space-between"
+
+}
+
+const styleEndButton = {
+    padding: "20px",
+    width: "100%",
+    borderRadius: "20px"
+}
+
+
+const ProfileForm = () => {
+    return (
+        <SectionAuthContainer title="Profil">
+            <VBox style={{ display: "flex" }}>
+                <InputTextDefault placeholder="Photo" style={styleInputText}/>
+                <InputTextDefault placeholder="Description" style={{...styleInputText, marginBottom : "20px"}}/>
+            </VBox>
+            <HBox gap="20px" style={styleOption}>
+                <InputTextDefault placeholder="Nom" style={{ marginBottom: "20px" }} />
+                <InputTextDefault placeholder="Prénom" style={{ marginBottom: "20px" }} />
+            </HBox>
+
+            <VBox style={{ display: "flex" }}>
+                <InputTextDefault placeholder="Email" style={styleInputText} />
+
+                    {/* <HBox gap="20px" style={styleOption}>
+                    <InputTextDefault placeholder="Numéro étudiant"/>
+                    <InputTextDefault placeholder="Numéro de téléphone"/>
+                    </HBox> */}
+                <StyledSelect style = {{marginBottom : "20px"}}>
+                    <option >Niveau d'étude</option>
+                    <option >L1</option>
+                    <option >L2</option>
+                    <option >L3</option>
+                    <option >M1</option>
+                    <option >M2</option>
+                    <option >Doctorat</option>
+                </StyledSelect>
+            </VBox>
+            <HBox gap="20px" style={styleOption}>
+                <InputTextDefault placeholder="École" style={{ marginBottom: "20px" }} />
+                <InputTextDefault placeholder="Ville" style={{ marginBottom: "20px" }} />
+            </HBox>
+            <VBox style={{ display: "flex" }}>
+                <InputTextDefault placeholder="Numéro de téléphone" style={styleInputText}/>
+                <InputTextDefault placeholder="Mot de passe" style={styleInputText} type="password"/>
+                <InputTextDefault placeholder="Confirmer mot de passe" style={{...styleInputText, marginBottom : "20px"}} type="password"/>
+            </VBox>
+
+            <CenterContainer>
+                <BasicButton style={{ styleEndButton }}>
+                    <h2 style={{ margin: "0" }}>S'enregistrer</h2>
+                </BasicButton>
+            </CenterContainer>
+
+     
+        </SectionAuthContainer>
+        
+        
+
+        
     )
 }
+export default ProfileForm;
--- frontend/src/pages/Signup/index.jsx
+++ frontend/src/pages/Signup/index.jsx
@@ -1,11 +1,9 @@
 import SignupForm from "../../components/Form/Signup";
-import NavbarOffset from "../../components/NavbarOffset";
 import CenterContainer from "../../containers/CenterContainer";
 
 function Signup() {
     return(
-        <CenterContainer style = {{minWidth: "500px"}}>  
-        <NavbarOffset></NavbarOffset>
+        <CenterContainer style = {{minWidth: "500px"}}> 
             <SignupForm></SignupForm>
         </CenterContainer>
     )
