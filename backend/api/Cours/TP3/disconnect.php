718298361c59d2b559c4620bffea0163dfa7dc99
CYTech Student <arthur.rimaudiere@free.fr>
2023-05-24 18:34:43 +0200

    patch 2 backend User

--- backend/api/Cours/TP3/disconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['login']);
-    $_SESSION['Statut-connection'] = false;
-    $_SESSION['produits'] = array();
-    header('Location: index.php');
-?>
