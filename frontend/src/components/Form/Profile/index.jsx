e3b525a47ac6339a49a96ababd992c6520238ff3
Hadronoxx <maxime_dejardin@orange.fr>
2023-05-24 14:42:33 +0200

    signup

---
+++ frontend/src/components/Form/Profile/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import InputTextDefault from "../../Input/Text/Default";
+import VBox from "../../../containers/VBox";
+import SectionAuthContainer from "../../../containers/SectionAuthContainer";
+import HBox from "../../../containers/HBox";
+import CenterContainer from "../../../containers/CenterContainer";
+import BasicButton from "../../BasicButton";
+import styled from "styled-components";
+
+
+const Profile = () => {
+    return(
+
+        <div style={{margin : "150px"}}>hello</div>
+
+    )
+}
