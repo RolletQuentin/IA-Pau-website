9e8cd36a69cd58bb3f90dace5fdff586407c213a
Quentin Rollet <rollet.quentin@orange.fr>
2023-05-24 23:24:14 +0200

    add admin pages

---
+++ frontend/src/pages/AdminRessources/index.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import NavbarOffset from "../../components/NavbarOffset";
+
+const StyledAdminRessources = styled.div``;
+
+function AdminRessources() {
+    return (
+        <StyledAdminRessources>
+            <NavbarOffset />
+            <div>AdminRessources</div>
+        </StyledAdminRessources>
+    );
+}
+
+export default AdminRessources;
