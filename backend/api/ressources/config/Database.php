0ff4bf13d01fa135c0f5ed6361ceda6a25eac559
SOULIER-Patrice <p.patrice976@hotmail.com>
2023-05-24 19:54:58 +0200

    ajout toutes fonctions Ressources

--- backend/api/ressources/config/Database.php
+++ backend/api/ressources/config/Database.php
@@ -2,11 +2,21 @@
 class Database{
     // Connexion à la base de données
     // Je récupère les informations de connexion dans le fichier BDDCredentials.php
-    private $host = "localhost";
-    private $db_name = "IA_Pau_database";
-    private $username = "user";
-    private $password = "password";
+    private $host;
+    private $db_name ;
+    private $username;
+    private $password;
     public $connexion;
+
+    //Modif pour utiliser le fichier config d'Arthur
+    public function __construct() {
+        include_once('../../utils/BDDCredentials.php');
+        $this->host = $servername;
+        $this->db_name = $dbname;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
 
     // getter pour la connexion
     public function getConnection(){
