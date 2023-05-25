7dc1ab10aecf87d1ae3283e5a7c4d247432329c0
CYTech Student <arthur.rimaudiere@free.fr>
2023-05-25 14:01:10 +0200

    patch backend

--- backend/api/user/login/index.php
+++ backend/api/user/login/index.php
@@ -1,9 +1,8 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
 
 use Firebase\JWT\JWT;
 
