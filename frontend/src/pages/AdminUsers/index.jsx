6edc47b7314b95ba0e0623626494b264025e05f3
Quentin Rollet <rollet.qunetin@orange.fr>
2023-05-25 10:48:03 +0200

    try to fetch data

--- frontend/src/pages/AdminUsers/index.jsx
+++ frontend/src/pages/AdminUsers/index.jsx
@@ -1,13 +1,20 @@
 import styled from "styled-components";
 import NavbarOffset from "../../components/NavbarOffset";
+import { useFetch } from "../../hooks/useFetch";
+import { Loader } from "../../utils/Atoms";
 
 const StyledAdminUsers = styled.div``;
 
 function AdminUsers() {
+    const { data, isLoading, error } = useFetch(
+        process.env.REACT_APP_PROXY + "/api/user/"
+    );
+    console.log(process.env.REACT_APP_PROXY);
+
     return (
         <StyledAdminUsers>
             <NavbarOffset />
-            <div>AdminUser</div>
+            {isLoading ? <Loader /> : <div>AdminUser</div>}
         </StyledAdminUsers>
     );
 }
