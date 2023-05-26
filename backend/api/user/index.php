77e41cd257527261e98b13e9a6847f401b0986cd
CYTech Student <arthur.rimaudiere@free.fr>
2023-05-26 10:13:41 +0200

    patch header status

--- backend/api/user/index.php
+++ backend/api/user/index.php
@@ -4,6 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('HTTP/1.1 200 OK');
 
 include '../utils/database.php';
 include 'methods.php';
