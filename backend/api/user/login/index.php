53699c635b8a837334cd6f6643946cc68b0a649f
CYTech Student <arthur.rimaudiere@free.fr>
2023-05-26 10:13:41 +0200

    patch header status

--- backend/api/user/login/index.php
+++ backend/api/user/login/index.php
@@ -1,17 +1,9 @@
 <?php
-// TEST PATRICE DEPANAGE
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-// ------------ FIN TEST
-
-
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('HTTP/1.1 200 OK');
 
 use Firebase\JWT\JWT;
 
