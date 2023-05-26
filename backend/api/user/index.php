a054748a30a46567ce71333b1a95536f14f90902
Quentin Rollet <rollet.qunetin@orange.fr>
2023-05-26 10:01:10 +0200

    admin pages

--- backend/api/user/index.php
+++ backend/api/user/index.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 error_reporting(E_ALL & ~E_DEPRECATED);
 ini_set("display_errors", 1);
 
