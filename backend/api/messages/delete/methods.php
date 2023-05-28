b08b7779f4ef6266314b63e6c5d1be74e1e17cba
CYTech Student <arthur.rimaudiere@free.fr>
2023-05-28 17:00:31 +0200

    Backend : Ajout des messages

--- backend/api/messages/delete/methods.php
+++ backend/api/messages/delete/methods.php
@@ -1,16 +1,25 @@
 <?php
+    require_once('../../../vendor/autoload.php');
     include_once('../../utils/permissionManager.php');
     include_once('../../utils/StringCorrection.php');
 
     include_once('../../utils/database.php');
     
-    function deleteTeam($token, $IdEquipe){
-
-        $IdEquipe = verifyStringToDatabaseInsertion($IdEquipe);
-
-        $role = getRoleFromJwt($token);
+    function deleteMessage($token, $IdMessage){
+        $IdMessage = verifyStringToDatabaseInsertion($IdMessage);
+        $role = getRoleFromJWT($token);
         $id = getUserIdFromJWT($token);
-        
+
+        $IdEquipe = -1;
+        $conn = getConnection();
+        $query = "SELECT * FROM Message WHERE IdMessage = " . $IdMessage . ";";
+        $result = mysqli_query($conn, $query);
+        if(mysqli_num_rows($result) > 0){
+            if($row = mysqli_fetch_array($result)){
+                $IdEquipe = $row["IdEquipe"];
+            }
+        }
+
         $IdEvenement = -1;
         $conn = getConnection();
         $query = "SELECT * FROM Equipe AS eq INNER JOIN Projet AS p ON p.IdProjet = eq.IdProjet INNER JOIN Evenement AS e ON e.IdEvenement = p.IdEvenement WHERE eq.IdEquipe = ". $IdEquipe . ";";
@@ -22,7 +31,7 @@
         }
         
         if($IdEvenement == -1){
-            throw new Exception ("Cette équipe est invalide car non reliée à un événement");
+            throw new Exception ("Ce message ou l'équipe reliée a celui ci n'existe plus !");
         }
 
         $hasPerm = false;
@@ -37,31 +46,19 @@
                 $typeOfPerm = "Not manager of this event";
             }
         } else if ($role == "Etudiant"){
-            $idLeader = getIdOfLeader($IdEquipe);
-            if($idLeader == $id){
-                $hasPerm = true;
-            } else {
-                $typeOfPerm = "Not leader of this event";
-            }
+
+            $typeOfPerm = "permission denied.";
         }
 
         if($hasPerm == false){
-            throw new Exception ("Vous n'avez pas la permission de supprimer cette équipe. (" . $typeOfPerm .")");
+            throw new Exception ("Vous ne pouvez pas supprimer ce message (" . $typeOfPerm .")");
         }
 
-        $conn = getConnection();
-        $query = "DELETE FROM Appartenir WHERE IdEquipe=". $IdEquipe .";";
-        mysqli_query($conn, $query);
-        $query = "DELETE FROM Message WHERE IdEquipe=". $IdEquipe .";";
-        mysqli_query($conn, $query);
-        $query = "DELETE FROM Equipe WHERE IdEquipe=". $IdEquipe .";";
+        $query = "DELETE FROM Message WHERE IdMessage = ". $IdMessage . ";";
         mysqli_query($conn, $query);
 
-        $array = array("sucess"=>true);
-        $json = json_encode($array);
-        mysqli_close($conn);
-        echo $json;
         http_response_code(200);
+        echo json_encode(array("sucess"=>true));
 
     }
 
---
+++ backend/api/messages/retrieve/index.php
@@ -0,0 +1,41 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE, PATCH");
+header('HTTP/1.1 200 OK');
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once('../../../vendor/autoload.php');
+
+include_once('methods.php');
+
+$header = apache_request_headers();
+$token = "";
+if(!(empty($header["Authorization"]))){
+    $token = str_replace("Bearer ", "", $header["Authorization"]);
+}
+
+try {
+
+    if($_SERVER['REQUEST_METHOD'] == "GET"){
+        if(!(empty($_GET["IdEquipe"]))){
+            retrieveMessage($token, $_GET["IdEquipe"]);
+        }
+    }
+
+} catch (Exception $e) {
+    $array = array(
+        "error"=>$e->getMessage()
+    );
+    $json = json_encode($array);
+    echo $json;
+    http_response_code(400);
+}
+
+?>
