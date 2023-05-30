9b0abd8b07ebd640aabe776ccd787b36d14e6a14
CYTech Student <arthur.rimaudiere@free.fr>
2023-05-30 19:13:44 +0200

    Sécurité anti double teams

--- backend/api/teams/join/methods.php
+++ backend/api/teams/join/methods.php
@@ -19,6 +19,43 @@
             if(mysqli_num_rows($result) > 0) {
                 throw new Exception ("L'utilisateur appartient déjà à l'équipe");
             }
+            $teamsOfUser = array();
+            $query = "SELECT * FROM Appartenir WHERE Identifiant = " . $IdUser . ";";
+            $result = mysqli_query($conn, $query);
+            if(mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_array($result)){
+                    array_push($teamsOfUser, $row["IdEquipe"]);
+                }
+            }
+            $IdEvement = -1;
+            $query = "SELECT * FROM Evenement as e INNER JOIN Projet AS p ON p.IdEvenement = e.IdEvenement INNER JOIN Equipe as eq ON eq.IdProjet = p.IdProjet WHERE IdEquipe = " . $IdEquipe . ";";
+            $result = mysqli_query($conn, $query);
+            if(mysqli_num_rows($result) > 0){
+                if($row = mysqli_fetch_array($result)){
+                    $IdEvement = $row["IdEvenement"];
+                }
+            }
+
+            if($IdEvement == -1){
+                throw new Exception ("Votre équipe n'est pas valide !");
+            }
+
+            $arrayOfTeamsInEvent = array();
+            $query = "SELECT * FROM Evenement as e INNER JOIN Projet AS p ON p.IdEvenement = e.IdEvenement INNER JOIN Equipe as eq ON eq.IdProjet = p.IdProjet WHERE e.IdEvenement = " . $IdEvenement . ";";
+            $result = mysqli_query($conn, $query);
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_array($result)){
+                    array_push($arrayOfTeamsInEvent, $row["IdEquipe"]);
+                }
+            }
+
+            foreach($teamsOfUser as $teamID){
+                if(in_array($teamID, $arrayOfTeamsInEvent)){
+                    throw new Exception ("Vous êtes déjà dans une équipe inscrite a cet événement !");
+                }
+            }
+            
+
             $query = "DELETE FROM Preinscription WHERE Identifiant = " . $IdUser . " AND IdEquipe = " . $IdEquipe . ";";
             mysqli_query($conn, $query);
             $query = "INSERT INTO Appartenir (Identifiant, IdEquipe) VALUES (". $IdUser . "," . $IdEquipe . ");";
