100a00798d503616958b57816da3640e93200d80
Quentin Rollet <rollet.quentin@orange.fr>
2023-05-30 08:31:55 +0200

    add admin user features

---
+++ frontend/src/pages/Admin/AdminAddUser/index.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import SignupForm from "../../../components/Form/Signup";
+import NavbarOffset from "../../../components/NavbarOffset";
+
+const StyledAddUser = styled.div`
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 500px;
+`;
+
+function AdminAddUser() {
+    return (
+        <StyledAddUser>
+            <NavbarOffset />
+            <h1>Ajouter un utilisateur</h1>
+            <div></div>
+            <SignupForm />
+        </StyledAddUser>
+    );
+}
+
+export default AdminAddUser;
