e50a54855750ede2f34678264899531df40d17f9
liro-u <86537787+liro-u@users.noreply.github.com>
2023-05-30 15:39:14 +0200

    css

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -4,9 +4,9 @@ import DataChallengeItem from "../../components/DataChallengeItem";
 import NavbarOffset from "../../components/NavbarOffset";
 import { useFetch } from "../../hooks/useFetch";
 import { Loader } from "../../utils/Atoms";
+import VBox from "../../containers/VBox";
 
 const StyledHome = styled.div`
-    border: 1px solid red;
     width: 500px;
     margin: auto;
     display: flex;
@@ -28,18 +28,11 @@ function Home() {
         <StyledHome>
             <NavbarOffset />
             <h2>Data Challenges</h2>
-            {/* {isLoading ? (
-                <Loader />
-            ) : (
-                <>
-                    {data.map(({ id, title }) => (
-                        <DataChallengeItem key={id} title={title} id={id} />
-                    ))}
-                </>
-            )} */}
-            <DataChallengeItem title="Data Challenge 1" id="1" />
-            <DataChallengeItem title="Data Challenge 2" id="2" />
-            <DataChallengeItem title="Data Challenge 3" id="3" />
+            <VBox>
+                <DataChallengeItem title="Data Challenge 1" id="1" />
+                <DataChallengeItem title="Data Challenge 2" id="2" />
+                <DataChallengeItem title="Data Challenge 3" id="3" />
+            </VBox>
         </StyledHome>
     );
 }
