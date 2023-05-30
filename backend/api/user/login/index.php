c21efbd988228e69282d89635fe0b7e5c39152a0
liro-u <86537787+liro-u@users.noreply.github.com>
2023-05-30 11:00:02 +0200

    patch mais pas sur

--- backend/api/user/login/index.php
+++ backend/api/user/login/index.php
@@ -6,6 +6,7 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE, PATCH");
 
+
 use Firebase\JWT\JWT;
 
 require_once('../../../vendor/autoload.php');
