20565503dc9f7c6752ccb7f2b66fa76b977dffbd
Quentin Rollet <rollet.qunetin@orange.fr>
2023-05-31 13:04:55 +0200

    try to fix bugs

--- frontend/src/toggles/togglePost.js
+++ frontend/src/toggles/togglePost.js
@@ -1,21 +1,41 @@
-import axios from "axios";
+// import axios from "axios";
+
+// export const togglePost = async (url, user, data) => {
+//     const config = {
+//         method: "post",
+//         maxBodyLength: Infinity,
+//         url: url,
+//         headers: {
+//             Authorization: `Bearer ${user.jwt}`,
+//             "Content-Type": "application/json",
+//         },
+//         data: JSON.stringify(data),
+//     };
+
+//     try {
+//         const response = await axios.request(config);
+//         console.log(response);
+//         return response.data;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// };
 
 export const togglePost = async (url, user, data) => {
-    const config = {
-        method: "post",
-        maxBodyLength: Infinity,
-        url: url,
-        headers: {
-            Authorization: `Bearer ${user.jwt}`,
-            "Content-Type": "application.json",
-        },
-        data: JSON.stringify(data),
-    };
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                Authorization: `Bearer ${user.jwt}`,
+                "Content-Type": "application/json",
+            },
+        });
 
-    axios
-        .request(config)
-        .then((response) => {
-            console.log(JSON.stringify(response.data));
-        })
-        .catch((error) => console.error(error));
+        const json = await response.json();
+        console.log(json);
+        return json;
+    } catch (error) {
+        console.error(error);
+    }
 };
