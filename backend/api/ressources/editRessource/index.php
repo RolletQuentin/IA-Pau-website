8b2a8f57472cc04951feb68d4da16c5b103c06f0
CYTech Student <arthur.rimaudiere@free.fr>
2023-06-01 21:58:44 +0200

    patch methods allowed cors

--- backend/api/ressources/editRessource/index.php
+++ backend/api/ressources/editRessource/index.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, method");
-header("Access-Control-Allow-Methods: PUT");
+header("Access-Control-Allow-Methods: PUT, PATCH");
 // --Pour fix les CORS 
 $method = $_SERVER['REQUEST_METHOD'];
 if ($method == "OPTIONS") {
