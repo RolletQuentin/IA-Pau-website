3b9cb51e162c89a3956abb224ea63e2100da6a5a
liro-u <86537787+liro-u@users.noreply.github.com>
2023-06-01 20:06:49 +0200

    patch

--- frontend/src/pages/DataChallengeDetail/index.jsx
+++ frontend/src/pages/DataChallengeDetail/index.jsx
@@ -105,11 +105,7 @@ function DataChallengeDetail() {
             }
         }
         const fetchHasTeam = async () => {
-            const response = await fetch(process.env.REACT_APP_PROXY + '/api/teams/me/?IdEvent=' + id, {
-                headers: {
-                    Authorization: `Bearer ${user.jwt}`,
-                },
-            })
+            const response = await fetch(process.env.REACT_APP_PROXY + '/api/teams/me/?IdEvent=' + id)
             const json = await response.json();
             
             if (response.ok) {
